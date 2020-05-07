7207b43043857ce0372267ac20293b2a30e83688
Janis Lebron <janislebron@gmail.com>
2020-05-07 00:21:30 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,10 @@
 #write your code here
-counter = 0
+countdown = 10
 while counter <
   time.now =- 1
   puts "#{number} SECOND(S)!"
   counter = 0 "HAPPY NEW YEAR!"
 def countdown
 end
+
+def countdown_with_sleep
